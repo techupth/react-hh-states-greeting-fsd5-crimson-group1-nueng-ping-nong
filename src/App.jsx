f52a1b1e46d35bb96d481665be6f628c1da4ae52
nongparanai <promlerd.p@gmail.com>
2023-12-21 13:37:31 +0700

    feat: add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+
+  const greetingThaiMessage = () => {
+    setGreetingMessage("สวัสดี!");
+  };
+
+  const greetingEnglishMessage = () => {
+    setGreetingMessage("Hi!");
+  };
+
+  const greetingChineseMessage = () => {
+    setGreetingMessage("你好!");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={greetingThaiMessage}>สวัสดี!</button>
+        <button onClick={greetingEnglishMessage}>Hi!</button>
+        <button onClick={greetingChineseMessage}>你好!</button>
       </div>
     </div>
   );
